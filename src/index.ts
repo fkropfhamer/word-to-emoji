6658dd3bbf461b9aef5b957113815ad9f207092b
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-07-22 15:17:17 +0200

    imported json and added random emoji function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import all_emojis from './all_emojis.json';
+
+
 function wordToEmoji() {
     console.log("❤️emojis❤️");
+}
+
+export function getRandomEmoji() {
+    return all_emojis[Math.floor(Math.random() * all_emojis.length)].symbol;
 }
   
 export default wordToEmoji;
